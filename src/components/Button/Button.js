1764f715772d5a478e0f9b9fddb6e12951e96fbb
Dasha <kokorina.darya1509@gmail.com>
2019-03-20 16:10:41 +0300

    button test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,17 @@
 import React from "react";
 import getClassName from "../../services/getClassName";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./Button.css";
 
 /**
  * Pretty modified button
  * */
 
-const Button = React.forwardRef(({className, size, customType, icon, children, ...otherProps}, ref) => (
+const Button = React.forwardRef(function Button(
+  { className, size, customType, icon, children, ...otherProps },
+  ref
+) {
+  return (
     <button
       className={getClassName(
         "Button",
@@ -17,16 +21,12 @@ const Button = React.forwardRef(({className, size, customType, icon, children, .
       ref={ref}
       {...otherProps}
     >
-      {icon && (
-        <span className="Button-Icon" >
-          {icon}
-        </span>
-      )}
+      {icon && <span className="Button-Icon">{icon}</span>}
 
       {children}
     </button>
-
-));
+  );
+});
 
 Button.propTypes = {
   size: PropTypes.oneOf(["small", "large"]),
@@ -34,7 +34,3 @@ Button.propTypes = {
 };
 
 export default Button;
-
-
-
-
---
+++ src/components/Button/__tests__/Button.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Button from "../Button";
+
+describe("Button", () => {
+  it("renders without errors", () => {
+    const wrapper = shallow(<Button />);
+
+    expect(wrapper.find(".Button")).toHaveLength(1);
+  });
+
+  it("renders with icon", () => {
+    const icon = <svg className="icon" />;
+    const wrapper = shallow(<Button icon={icon} />);
+
+    expect(wrapper.find(".Button-Icon")).toHaveLength(1);
+    expect(wrapper.contains(<span className="Button-Icon">
+      {icon}
+      </span>)).toBeTruthy();
+  });
+
+  it("renders with dependencies", () => {
+    const wrapper = shallow(<Button size="small" customType="ghost" />);
+
+    expect(
+      wrapper.hasClass("Button_customType_ghost")
+    ).toBeTruthy();
+  });
+
+  it("renders with text", () => {
+    const wrapper = shallow(<Button>Default</Button>);
+
+    expect(wrapper.contains("Default")).toEqual(true);
+  });
+});
