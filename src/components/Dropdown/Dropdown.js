dffc5bc1d5f6a328c606c6a40a7f939902e44ed7
Dasha <kokorina.darya1509@gmail.com>
2019-03-25 15:03:26 +0300

    +focus on Dropdown +open & close Dropdowns by keys +key up and down on Dropdown items

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -18,8 +18,55 @@ export default class Dropdown extends Component {
       });
     };
 
-    this.selectItemHandler = data => {
-      this.setState({ textSelect: data });
+    this.keyEnterHandler = e => {
+      if (e.keyCode === 13) {
+        this.setState(() => {
+          return { display: "show" };
+        });
+      }
+      if (e.keyCode === 27) {
+        this.setState(() => {
+          return { display: "hide" };
+        });
+      }
+    };
+
+    this.selectItemHandler = e => {
+      this.setState({ textSelect: e.target.textContent });
+      const variants = document.getElementById("listOfItems");
+      const toChangeElem = variants.getElementsByClassName(
+        "Dropdown-Options-Item_selected"
+      );
+      toChangeElem[0].classList.remove("Dropdown-Options-Item_selected");
+      e.target.classList.add("Dropdown-Options-Item_selected");
+    };
+
+    this.keyArrowHandler = e => {
+      let variants = null;
+      let innerElems = null;
+      let arrFromCollection = null;
+      let selectedIndex = null;
+      if (e.keyCode === 40 || e.keyCode === 38) {
+        variants = document.getElementById("listOfItems");
+        innerElems = variants.getElementsByClassName("Dropdown-Select");
+        console.log(innerElems);
+       arrFromCollection = Array.from(innerElems);
+
+       arrFromCollection.forEach((item, index) => {
+          if (item.classList.contains("Dropdown-Options-Item_selected")) {
+           selectedIndex = index;
+           console.log(selectedIndex);
+          }
+        });
+      }
+     if (e.keyCode === 40 && selectedIndex !== arrFromCollection.length-1) {
+      innerElems[selectedIndex].classList.remove("Dropdown-Options-Item_selected");
+      innerElems[selectedIndex+1].classList.add("Dropdown-Options-Item_selected");
+     }
+     if (e.keyCode === 38 && selectedIndex !== 0) {
+      innerElems[selectedIndex].classList.remove("Dropdown-Options-Item_selected");
+      innerElems[selectedIndex-1].classList.add("Dropdown-Options-Item_selected");
+     }
     };
   }
 
@@ -37,13 +84,17 @@ export default class Dropdown extends Component {
 
     if (data) {
       optionsList = data.map((item, index) => {
+        let classNameValue = "Dropdown-Select Dropdown-Options-Item";
+        if (index === 0) {
+          classNameValue =
+            "Dropdown-Select Dropdown-Options-Item Dropdown-Options-Item_selected";
+        }
         return (
           <li
-            className="Dropdown-Select Dropdown-Options-Item"
+            id={`id_${index}`}
+            className={classNameValue}
             key={`key_${index}`}
-            onClick={() => this.selectItemHandler(item)}
-            role="option"
-            tabIndex={index}
+            onClick={this.selectItemHandler}
           >
             {item}
           </li>
@@ -53,20 +104,23 @@ export default class Dropdown extends Component {
 
     return (
       <div
-       className="Dropdown Dropdown_shadow" onClick={this.showOptionsHandler}
-      tabIndex={1}>
-        <div className="Dropdown-SelectImage"
-        aria-haspopup="listbox">
-          {selectIcon}
-        </div>
-        <div className={`Dropdown-Select ${classSelectValue}`}>
+        className="Dropdown Dropdown_shadow"
+        onClick={this.showOptionsHandler}
+      >
+        <div className="Dropdown-SelectImage">{selectIcon}</div>
+        <div
+          className={`Dropdown-Select ${classSelectValue}`}
+          tabIndex="0"
+          onKeyDown={this.keyEnterHandler}
+        >
           {textSelect}
         </div>
         <div className="Dropdown-Icon">{icon}</div>
         <ul
+          id="listOfItems"
           className={`Dropdown Dropdown-Options Dropdown_shadow Dropdown-Options_${display}`}
-          role="listbox"
-          tabIndex="-1"
+          tabIndex="0"
+          onKeyDown={this.keyArrowHandler}
         >
           {optionsList}
         </ul>
